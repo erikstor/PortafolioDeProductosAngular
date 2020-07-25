097b9cdb8a5d0e894e430ec76b0223d82cc0e6d2
Erik Fernando Stor Franco <erik.stor.franco@gmail.com>
2020-07-25 15:22:10 -0500

    Adicion de firebase

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,14 +9,31 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo:any[] = [];
 
   constructor(private _http: HttpClient) { 
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
+
+  private cargarInfo(){
     this._http.get('assets/data/data-pagina.json')
     .subscribe((resp: InfoPagina) => {
       this.cargada = true;
       this.info = resp;      
     });
-
   }
+
+  private cargarEquipo(){
+    this._http.get('https://erik-stor-page.firebaseio.com/equipo.json')
+    .subscribe((resp: any) => {
+      
+      this.equipo = resp;
+      console.log(resp);
+      
+    });
+  }
+
+
 }
